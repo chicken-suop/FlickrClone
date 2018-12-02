91638d74a8d4f0be8c1b14ebf4f602f19f1cedfc
ratskin <elliot.schep@gmail.com>
2018-12-02 22:16:30 +0100

    Removed extraneous code in app.js

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
-import { matchPath } from 'react-router';
 import routes from '../routes';
-import { searchPhotos, getDetail } from '../helpers/fetch';
 import preloadedDataPropType from '../helpers/preloadedDataPropType';
 
 const Main = styled.div`
@@ -37,75 +35,25 @@ const Main = styled.div`
   }
 `;
 
-class AppContainer extends React.Component {
-  static propTypes = {
-    preloadedData: preloadedDataPropType.isRequired,
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = { preloadedData: props.preloadedData };
-  }
-
-  // componentWillReceiveProps(nextProps) {
-  //
-  //   // If we change routes without SSR, clear old data
-  //   const { location } = this.props;
-  //   if (nextProps.location.pathname !== location.pathname) {
-  //     const isFeed = nextProps.location.pathname === '/feed';
-  //     const isDetail = !!matchPath(
-  //       nextProps.location.pathname,
-  //       '/feed/:id',
-  //     );
-  //
-  //     if (isFeed) {
-  //       searchPhotos({})
-  //         .then(preloadedData => this.setState({ preloadedData }));
-  //     } else if (isDetail) {
-  //       const id = nextProps.location.pathname.match(/^\/feed\/(\d+)/)[1];
-  //       getDetail(id)
-  //         .then(preloadedData => this.setState({ preloadedData }));
-  //     }
-  //   }
-  // }
-
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   const { preloadedData } = this.state;
-  //   return preloadedData.length !== nextState.preloadedData.length;
-  // }
-
-  render() {
-    const { preloadedData } = this.state;
-    const { location } = this.props;
-
-    return (
-      <Main>
-        <Switch location={location}>
-          {routes.map(route => (
-            <Route
-              key={route.id}
-              path={route.path}
-              component={props => (
-                <route.component
-                  preloadedData={preloadedData}
-                  {...props}
-                />
-              )}
-              exact
-            />
-          ))}
-        </Switch>
-      </Main>
-    );
-  }
-}
-
 const App = ({ preloadedData }) => (
   <Route render={({ location }) => (
-    <AppContainer
-      preloadedData={preloadedData}
-      location={location}
-    />
+    <Main>
+      <Switch location={location}>
+        {routes.map(route => (
+          <Route
+            key={route.id}
+            path={route.path}
+            component={props => (
+              <route.component
+                preloadedData={preloadedData}
+                {...props}
+              />
+            )}
+            exact
+          />
+        ))}
+      </Switch>
+    </Main>
   )}
   />
 );
